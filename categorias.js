3f1af3cba70b4153ef5d835b65bbbfea6ba43670
Wallysson Dos Santos Freitas <wallymaxxx456@gmail.com>
2025-01-25 20:00:05 -0300

    Update categorias.js
    adicionei  um botão de apagar categorias

--- categorias.js
+++ categorias.js
@@ -1,18 +1,27 @@
 $('#criar').click(function(){
     let nomecat = $('#nomecategoria').val();
-    $('#organizar').append('<br> <input type="button"' + 'value="' + nomecat + '" class="categorias ' + nomecat + '">');
-    $('#organizar').append('<div id ="'+ nomecat + '" class="textos"> </div>');
-    $('#'+nomecat).css("display","none");
+    $('#organizar').append(`
+        <div id="categoria-${nomecat}" class="categoria">
+            <input type="button" value="${nomecat}" class="categorias ${nomecat}">
+            <button class="apagarCategoria" data-categoria="${nomecat}">Apagar</button>
+        </div>
+    `);
+    $('#organizar').append('<div id="' + nomecat + '" class="textos" style="display:none;"> </div>');
+    $('#nomecategoria').val('');
 });
 
 let chave;
-
-$('.categorias').click(function(e){
+$('#organizar').on('click', '.categorias', function(e){
     if (chave) {
-        $('.textos').css("display","none");
+        $('.textos').css("display", "none");
     }
-    let botao = $(e.currentTarget).value;
-    $('#'+botao).css("display", "inline");
+    let botao = $(e.currentTarget).val(); 
+    $('#' + botao).css("display", "inline");
     chave = 1;
-})
+});
 
+$('#organizar').on('click', '.apagarCategoria', function(){
+    let nomecat = $(this).data('categoria');  
+    $('#categoria-' + nomecat).remove();
+    $('#' + nomecat).remove();
+});
