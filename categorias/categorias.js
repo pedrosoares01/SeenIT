b6b03c81e186cde04f26eb0f0856ecf9a06c21c9
pedrosoares01 <pedro.soares.souto@gmail.com>
2025-01-27 16:54:26 -0300

    fiz o sistema de filtragem

--- categorias/categorias.js
+++ categorias/categorias.js
@@ -1,5 +1,35 @@
 $('.filme').click(function(e){
     let $cat = $(e.currentTarget);
+    $cat.toggleClass('nvisto');
     $cat.toggleClass('visto');
 });
 
+$('.filtro').click(function(){
+    $('.assistidos').toggleClass('aberto')
+    $('.naoassistidos').toggleClass('aberto');
+    $('.tudo').toggleClass('aberto');
+})
+
+$('.naoassistidos').click(function(){
+    if ($('.nvisto').hasClass('escondido')) {
+        $('.nvisto').toggleClass('escondido');
+    }
+    $('.visto').toggleClass('escondido');
+    
+})
+
+$('.assistidos').click(function(){
+    if ($('.visto').hasClass('escondido')) {
+        $('.visto').toggleClass('escondido');
+    }
+    $('.nvisto').toggleClass('escondido');
+})
+
+$('.tudo').click(function(){
+    if ($('.visto').hasClass('escondido')) {
+        $('.visto').toggleClass('escondido');
+    }
+    if ($('.nvisto').hasClass('escondido')) {
+        $('.nvisto').toggleClass('escondido');
+    }
+})
